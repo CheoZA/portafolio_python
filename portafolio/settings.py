e3398b7495255128b1ef71c9cf6e10ace9663fd3
Juan Felipe Garza Sánchez <fegarza@outlook.es>
2021-12-02 22:07:30 -0600

    fix: se remueve hotreload

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'livereload',
     'home',
     'blog'
 ]
@@ -50,8 +49,7 @@ MIDDLEWARE = [
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'livereload.middleware.LiveReloadScript'
+    'django.middleware.clickjacking.XFrameOptionsMiddleware'
 ]
 
 ROOT_URLCONF = 'portafolio.urls'
